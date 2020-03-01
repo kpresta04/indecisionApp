9976e0e66f2af5658154efa1e799a350d9671cd9
Kellen Presta <kpresta04@gmail.com>
2020-02-29 22:13:55 -0600

    66 added normalize

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionAPP from "./components/IndecisionApp";
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 ReactDOM.render(<IndecisionAPP />, document.querySelector("#app"));
