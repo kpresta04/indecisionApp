995fa7ff2fbb108173cb437a60bbaaa7ea60627b
Kellen Presta <kpresta04@gmail.com>
2020-03-02 20:46:48 -0600

    71

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,6 +6,8 @@ const OptionModal = props => (
     isOpen={!!props.selectedOption}
     contentLabel={"Selected Option"}
     onRequestClose={props.handleClearSelectedOption}
+    closeTimeoutMS={200}
+    className="modal"
   >
     <h3>Selected Option</h3>
     {props.selectedOption && <p>{props.selectedOption}</p>}
